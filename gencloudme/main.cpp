6796c64be77c48f949d9d5f0dcfc544dac3c153b
Arseniy <pashatun228@gmail.com>
2020-09-17 10:31:56 +0300

    Initial Commit

---
+++ gencloudme/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  gencloudme
+//
+//  Created by Arsenii Klevakin on 17.09.2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
